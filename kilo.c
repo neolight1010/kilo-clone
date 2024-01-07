7d6def9706e14dcb1011670c4278f132ca2a9125
Anthony Suárez <neolight1010@gmail.com>
2024-01-07 18:24:09 -0500

    Modify miscellaneous flags

--- kilo.c
+++ kilo.c
@@ -13,7 +13,8 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_cflag |= (CS8);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
