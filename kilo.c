585cea21fde13e5c116e3d77e835977c8d1ebf55
Anthony Suárez <neolight1010@gmail.com>
2024-01-13 17:38:03 -0500

    Reposition on every loop

--- kilo.c
+++ kilo.c
@@ -74,7 +74,10 @@ void editorProcessKeypress() {
 
 /*** output ***/
 
-void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
 /*** init ***/
 
