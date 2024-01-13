fc7fda2a774579f3b3397c17ae362777a46ca717
Anthony Suárez <neolight1010@gmail.com>
2024-01-13 17:39:48 -0500

    Reposition cursor after exit

--- kilo.c
+++ kilo.c
@@ -17,6 +17,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -67,6 +70,9 @@ void editorProcessKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     exit(0);
     break;
   }
