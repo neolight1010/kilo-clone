379b0f2c9db1c208e5a890ebe01f557054541812
Anthony Suárez <neolight1010@gmail.com>
2024-03-02 16:06:36 -0500

    Clear each line before refreshing

--- kilo.c
+++ kilo.c
@@ -162,6 +162,7 @@ void editorDrawRows(struct abuf *ab) {
   for (int y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3); // Clear line
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -172,7 +173,6 @@ void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6); // Hide cursor
-  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
