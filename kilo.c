7d1dc6607ab3c81e1978dee2a715b3573d1f33b6
Anthony Suárez <neolight1010@gmail.com>
2024-03-02 15:32:56 -0500

    Implement fallback method to get window size

--- kilo.c
+++ kilo.c
@@ -70,11 +70,47 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  char buf[32];
+  unsigned int i = 0;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+      break;
+    }
+
+    if (buf[i] == 'R') {
+      break;
+    }
+
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') {
+    return -1;
+  }
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+    return -1;
+  }
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+      return -1;
+    }
+
+    return getCursorPosition(rows, cols);
   } else {
     *rows = ws.ws_row;
     *cols = ws.ws_col;
