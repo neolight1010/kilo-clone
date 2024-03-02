2b566004d6078e0dcac3b8cfb3051aace38a5efd
Anthony Suárez <neolight1010@gmail.com>
2024-03-02 17:21:55 -0500

    Read Home and End keys

--- kilo.c
+++ kilo.c
@@ -20,7 +20,9 @@ enum editorKey {
   ARROW_UP,
   ARROW_DOWN,
   PAGE_UP,
-  PAGE_DOWN
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY
 };
 
 /*** data ***/
@@ -98,10 +100,18 @@ int editorReadKey() {
 
         if (seq[2] == '~') {
           switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '4':
+            return END_KEY;
           case '5':
             return PAGE_UP;
           case '6':
             return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
           }
         }
       } else {
@@ -114,7 +124,18 @@ int editorReadKey() {
           return ARROW_RIGHT;
         case 'D':
           return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
         }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
       }
     }
 
@@ -239,6 +260,13 @@ void editorProcessKeypress() {
       editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
     }
   } break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+  case END_KEY:
+    E.cx = E.screencols - 1;
+    break;
 
   case ARROW_UP:
   case ARROW_DOWN:
