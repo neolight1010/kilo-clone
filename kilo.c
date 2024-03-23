95d6922022b6a2b8b043fcdfe58c87a024fa2f3c
Anthony Suárez <neolight1010@gmail.com>
2024-03-23 14:50:14 -0500

    Show status bar

--- kilo.c
+++ kilo.c
@@ -53,6 +53,8 @@ struct editorConfig {
 
   int numrows;
   erow *row;
+
+  char *filename;
 };
 
 struct editorConfig E;
@@ -280,6 +282,9 @@ void editorAppendRow(char *s, size_t len) {
 /*** file i/o **/
 
 void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
   FILE *fp = fopen(filename, "r");
   if (!fp) {
     die("fopen");
@@ -474,10 +479,35 @@ void editorDrawRows(struct abuf *ab) {
     }
 
     abAppend(ab, "\x1b[K", 3); // Clear line
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
+    abAppend(ab, "\r\n", 2);
+  }
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[7m", 4); // Invert colors
+
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+                     E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+
+  if (len > E.screencols) {
+    len = E.screencols;
+  }
+
+  abAppend(ab, status, len);
+
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
     }
   }
+
+  abAppend(ab, "\x1b[m", 3); // Reset formatting
 }
 
 void editorRefreshScreen() {
@@ -489,6 +519,7 @@ void editorRefreshScreen() {
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
@@ -511,10 +542,13 @@ void initEditor() {
   E.rowoff = 0;
   E.coloff = 0;
   E.row = NULL;
+  E.filename = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
     die("getWindowSize");
   }
+
+  E.screenrows -= 1;
 }
 
 int main(int argc, char *argv[]) {
