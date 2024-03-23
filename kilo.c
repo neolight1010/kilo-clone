e57f5649723afcd33bd98c27c926a55736ec054b
Anthony Suárez <neolight1010@gmail.com>
2024-03-23 14:20:44 -0500

    Render tabs as spaces

--- kilo.c
+++ kilo.c
@@ -15,6 +15,7 @@
 /*** defined ***/
 
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -35,6 +36,9 @@ enum editorKey {
 typedef struct erow {
   int size;
   char *chars;
+
+  int rsize;
+  char *render;
 } erow;
 
 struct editorConfig {
@@ -210,7 +214,35 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
-/*** editorAppendRow ***/
+/*** row operations ***/
+
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+
+  for (int j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      tabs++;
+    }
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (int j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) {
+        row->render[idx++] = ' ';
+      }
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
 
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -220,6 +252,11 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -394,7 +431,7 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
       if (len < 0) {
         len = 0;
       }
@@ -402,7 +439,7 @@ void editorDrawRows(struct abuf *ab) {
         len = E.screencols;
       }
 
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3); // Clear line
