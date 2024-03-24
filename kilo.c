c3fd81801dc5cf9cca74872303f11f1548408e7c
Anthony Suárez <neolight1010@gmail.com>
2024-03-24 10:56:05 -0500

    Ignore inserting special characters

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -402,6 +403,10 @@ void editorProcessKeypress() {
   int c = editorReadKey();
 
   switch (c) {
+  case '\r':
+    // TODO
+    break;
+
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -433,11 +438,21 @@ void editorProcessKeypress() {
     E.cx = E.row[E.cy].size;
     break;
 
+  case BACKSPACE:
+  case CTRL_KEY('h'):
+  case DEL_KEY:
+    // TODO
+    break;
+
   case ARROW_UP:
   case ARROW_DOWN:
   case ARROW_LEFT:
   case ARROW_RIGHT:
     editorMoveCursor(c);
+    break;
+
+  case CTRL_KEY('l'):
+  case '\x1b': // Escape key
     break;
 
   default:
