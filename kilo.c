d5b34b06a44e7fe486151ddc3c9611bb6a073467
Anthony Suárez <neolight1010@gmail.com>
2024-07-12 23:27:47 -0500

    Implement search directions

--- kilo.c
+++ kilo.c
@@ -504,16 +504,41 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+  
   if (key == '\r' || key == '\x1b') { // enter or escape
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
+  if (last_match == -1) {
+    direction = 1;
+  }
+
+  int current = last_match;
   for (int i = 0; i < E.numrows; i++) {
-    erow *row = &E.row[i];
+    current += direction;
+    if (current == -1) {
+      current = E.numrows - 1;
+    } else if (current == E.numrows) {
+      current = 0;
+    }
+    
+    erow *row = &E.row[current];
     char *match = strstr(row->render, query);
 
     if (match) {
-      E.cy = i;
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
@@ -526,8 +551,8 @@ void editorFind() {
   int saved_cy = E.cy;
   int saved_coloff = E.coloff;
   int saved_rowoff = E.rowoff;
-  
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
 
   if (query) {
     free(query);
@@ -915,4 +940,5 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-// TODO https://viewsourcecode.org/snaptoken/kilo/06.search.html : Restore cursor position when cancelling search
+// TODO https://viewsourcecode.org/snaptoken/kilo/06.search.html : Restore
+// cursor position when cancelling search
