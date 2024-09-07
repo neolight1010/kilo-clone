648f6f0f9a6a4a2aed2abf2fa90790645a790aed
Anthony Suárez <neolight1010@gmail.com>
2024-09-06 21:49:58 -0500

    Highlight comments

--- example.c
+++ example.c
@@ -1,2 +1,3 @@
 test c file "hello"
+// this is a comment
 1234 234,252
--- kilo.c
+++ kilo.c
@@ -37,7 +37,13 @@ enum editorKey {
   DEL_KEY
 };
 
-enum editorHighlight { HL_NORMAL = 0, HL_STRING, HL_NUMBER, HL_MATCH };
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_COMMENT,
+  HL_STRING,
+  HL_NUMBER,
+  HL_MATCH
+};
 
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
 #define HL_HIGHLIGHT_STRINGS (1 << 1)
@@ -47,6 +53,7 @@ enum editorHighlight { HL_NORMAL = 0, HL_STRING, HL_NUMBER, HL_MATCH };
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -90,7 +97,7 @@ struct editorConfig E;
 char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 
 struct editorSyntax HLDB[] = {
-    {"c", C_HL_extensions, HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
+    {"c", C_HL_extensions, "//", HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
@@ -275,6 +282,9 @@ void editorUpdateSyntax(erow *row) {
     return;
   }
 
+  char *scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
   int in_string = 0;
 
@@ -282,6 +292,13 @@ void editorUpdateSyntax(erow *row) {
   while (i < row->size) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string) {
+      if (!strncmp(&row->render[i], scs, scs_len)) {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - 1);
+        break;
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
       if (in_string) {
@@ -308,7 +325,7 @@ void editorUpdateSyntax(erow *row) {
           continue;
         }
       }
-    } 
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
       if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -325,9 +342,10 @@ void editorUpdateSyntax(erow *row) {
   }
 }
 
-
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_COMMENT:
+      return 36;
   case HL_STRING:
     return 35;
   case HL_NUMBER:
@@ -359,7 +377,7 @@ void editorSelectSyntaxHighlight() {
         for (int filerow = 0; filerow < E.numrows; filerow++) {
           editorUpdateSyntax(&E.row[filerow]);
         }
-        
+
         return;
       }
 
